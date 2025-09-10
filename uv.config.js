38f452608b1900eb53e5423169d4480d34e5babe
mehhhofficial <78826290+mehhhofficial@users.noreply.github.com>
2025-09-09 20:44:12 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -7,7 +7,8 @@ self.__uv$config = {
   handler: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.handler.js",
   client: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.client.js",
   bundle: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.bundle.js",
-  config: "https://cdn.jsdelivr.net/gh/mehhhofficial/supreme-dollop@latest/uv.config.js",
+  config: "https://cdn.jsdelivr.net/gh/mehhhofficial/supreme-dollop@v2/uv.config.js",
   sw: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.sw.js",
 
 };
+
